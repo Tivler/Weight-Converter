7e1303b625e0d5afb7cd412558e3291cebf4ce27
tivler <thomasdmckyer@gmail.com>
2019-02-08 21:43:36 -0500

    created converter

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 
 <script>
 import Converter from './components/converter'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 export default {
   name: 'App',
