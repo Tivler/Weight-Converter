8f0b3ab905c57fc0dbbc268358a6ccc6a867da79
tivler <thomasdmckyer@gmail.com>
2019-02-08 20:29:35 -0500

    fixed routing and changed the name of component from helloWorld to converter

---
+++ src/components/converter.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="hero">
+  <h1>hey</h1>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
